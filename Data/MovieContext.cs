bf9e261689b03c2282dc0c7f0b2b3a78aaaaa454
Gvaurdin <gvaurdin777@yandex.ru>
2024-09-05 20:44:04 +0300

    Добавьте файлы проекта.

---
+++ Data/MovieContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTestCRUDMethodsForDB.Models;
+
+namespace MovieTestCRUDMethodsForDB.Data
+{
+    public class MovieContext(DbContextOptions<MovieContext> options) : DbContext(options)
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
